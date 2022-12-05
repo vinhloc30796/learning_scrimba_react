3c315be57ccf7cbdd0db2242b1e3cff9d85c3a43
Loc Nguyen <vinhloc30796@gmail.com>
2022-12-05 18:49:35 +0800

    Content

--- proj1_business_card/src/components/About.js
+++ proj1_business_card/src/components/About.js
@@ -3,6 +3,23 @@ import React from "react"
 export default function About() {
     return <div className="flex flex-col">
         <h2 className="text-white text-3xl font-bold mt-3">About</h2>
-        <p className="text-slate-300 mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p className="text-slate-300 mt-2">
+            I'm a marketer turned data & web engineer with a strong interest in financial equality. My international experiences have exposed me to both high-level thinking and low-level operational excellence. Rough timeline:
+        </p>
+        <ul className="list-disc list-inside text-slate-300">
+            <li>
+                <span className="font-bold">Curr:</span>
+                <ul className="ml-3 list-disc list-inside text-slate-300">
+                    <li>Sr Data Engineer (Protocol Labs)</li> 
+                    <li>Nanyang Technological University (MSc Fintech - May '23)</li>
+                </ul>
+            </li>
+            <li>
+                <span className="font-bold">Short-term: </span>DeFi Engineer
+            </li>
+            <li>
+            <span className="font-bold">Long-term: </span>APAC Web3 Pro-sumer Startup
+            </li>
+        </ul>
     </div>
 }
--- proj1_business_card/src/components/Interests.js
+++ proj1_business_card/src/components/Interests.js
@@ -3,6 +3,8 @@ import React from "react"
 export default function Interests() {
     return <div>
         <h2 className="text-white text-3xl font-bold mt-3">Interests</h2>
-        <p className="text-slate-300 mt-2 mb-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p className="text-slate-300 mt-2 mb-12">
+            Web3 (DAO Ops, DeFi, Infra); Fintech (Payments, Wealth, Trading); ML (MLOps, Time series, Tabular); Full-stack (FastAPI, React, Next)
+        </p>
     </div>
 }
